7b93aab0bad77ed89f0db2f85f3423fca8e44555
Ozzmosis94 <ozzmosis94@googlemail.com>
2020-10-19 22:00:37 +0300

    вытащил функцию

--- pages/doctors.js
+++ pages/doctors.js
@@ -1,4 +1,15 @@
-import getList from './getList()';
+import doctors from './doctors_array.js';
+import Doctors from './DoctorsComp.js';
+
+function getList() {
+    return(
+        <div className="doctor-list">
+            {doctors.map(item => <Doctors doc={item.doc} price={item.price}/>)}            
+        </div>
+        
+    );
+}
+
 
 export default function docs() {
     return (
